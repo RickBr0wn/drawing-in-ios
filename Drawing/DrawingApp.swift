162dc7b9ca9eda9e5e0f342d9a392c1f8d75f89e
Rick Brown <ricky.brown.00@gmail.com>
2021-04-06 18:00:39 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Rick Brown on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
